5cc16cb220528e89bc08c9dd8833d6ff74c7b186
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-05-11 00:44:03 -0500

    Add vehicle form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, VehicleFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
