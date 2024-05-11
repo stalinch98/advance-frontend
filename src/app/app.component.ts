1ed7047bf1155ef4529933f449bab07dcab97ec8
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-05-11 15:52:50 -0500

    chore: Remove vehicle form component and related files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { PicoPlacaFormComponent } from './pico-placa-form/pico-placa-form.component';
 import { RouterOutlet } from '@angular/router';
-import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, VehicleFormComponent, PicoPlacaFormComponent, HeaderComponent, HttpClientModule],
+  imports: [RouterOutlet, FooterComponent, PicoPlacaFormComponent, HeaderComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
